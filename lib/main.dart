193c3e440cf5d0cdb4050b9722e9b373991b5bba
akash shiv <akashshiv34@gmail.com>
2022-12-13 20:01:25 +0530

    firt commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import'package:flutter/material.dart';
+
+ void main() {
+  runApp(myApp());
+  
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Material(
+        child: Center(
+        child : Container(
+               child: Text("First Flutter App go again"),
+    ),
+    ),
+    ),
+    );
+  
+  }
+}
